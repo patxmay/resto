ce9ffb8a75a3df35e64322bb6925cca7d6617d83
mapluzdell <mapluzdell@mapluzdell-Latitude-5550.com>
2025-05-10 20:03:57 +0200

    controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,41 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\Restaurant;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
 
-class RestaurantController extends Controller
+class Controller extends BaseController
 {
-    // Recherche de restaurants
-    public function search(Request $request)
-    {
-        $query = Restaurant::query();
-
-        if ($request->has('name')) {
-            $query->where('name', 'like', $request->exact ? $request->name : '%' . $request->name . '%');
-        }
-
-        if ($request->has('address')) {
-            $query->where('address', 'like', '%' . $request->address . '%');
-        }
-
-        if ($request->has('cuisine')) {
-            $query->where('cuisine', $request->cuisine);
-        }
-
-        if ($request->has('rating_min') && $request->has('rating_max')) {
-            $query->whereBetween('rating', [$request->rating_min, $request->rating_max]);
-        }
-
-        $restaurants = $query->get();
-
-        return view('restaurants.search', ['restaurants' => $restaurants]);
-    }
-
-    // Afficher la fiche d'un restaurant
-    public function show($id)
-    {
-        $restaurant = Restaurant::findOrFail($id);
-        return view('restaurants.show', ['restaurant' => $restaurant]);
-    }
+    use AuthorizesRequests, ValidatesRequests;
 }
--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,44 @@
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
+Routes publiques (accessibles aux visiteurs)
 */
+use App\Http\Controllers\RestaurantController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [RestaurantController::class, 'search'])->name('restaurants.search'); // Page de recherche de restaurants
+Route::get('/restaurants/{id}', [RestaurantController::class, 'show'])->name('restaurants.show'); // Fiche d'un restaurant
+Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register'); // Formulaire d'inscription
+Route::post('/register', [AuthController::class, 'register']); // Soumission du formulaire d'inscription
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login'); // Formulaire de connexion
+Route::post('/login', [AuthController::class, 'login']); // Soumission du formulaire de connexion
+
+/*
+ Routes protégées (accessibles uniquement aux utilisateurs authentifiés)
+*/
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ReviewController;
+
+Route::middleware('auth')->group(function () {
+    // Profil utilisateur
+    Route::get('/profile', [UserController::class, 'profile'])->name('profile'); // Page du profil
+    Route::post('/profile', [UserController::class, 'updateProfile']); // Mise à jour du profil
+
+    // Critiques
+    Route::post('/reviews', [ReviewController::class, 'store'])->name('reviews.store'); // Ajouter une critique
+    Route::get('/reviews/{id}/edit', [ReviewController::class, 'edit'])->name('reviews.edit'); // Modifier une critique
+    Route::put('/reviews/{id}', [ReviewController::class, 'update'])->name('reviews.update'); // Enregistrer les modifications
+    Route::delete('/reviews/{id}', [ReviewController::class, 'destroy'])->name('reviews.destroy'); // Supprimer une critique
 });
+
+/*
+ Routes pour les modérateurs
+ */
+
+Route::middleware(['auth', 'role:moderator'])->group(function () {
+    Route::get('/moderation', [ReviewController::class, 'moderation'])->name('reviews.moderation'); // Interface de modération
+    Route::post('/moderation/{id}/validate', [ReviewController::class, 'validateReview'])->name('reviews.validate'); // Valider une critique
+    Route::post('/moderation/{id}/invalidate', [ReviewController::class, 'invalidateReview'])->name('reviews.invalidate'); // Invalider une critique
+});
+
+
+
